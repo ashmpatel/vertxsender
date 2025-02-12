7eccdd379b4b4185baedeeadf82285898d96a93a
Ashish Patel <ashmpatel@gmail.com>
2025-02-12 15:08:38 +0000

    updated to receive on different points
    updated to receive on different points

--- src/main/java/io/vertx/ClusteredSender.java
+++ src/main/java/io/vertx/ClusteredSender.java
@@ -23,6 +23,8 @@ public class ClusteredSender extends AbstractVerticle
     public void start(Promise<Void> promise) {
         final Router router = RouterHelper.createRouter(vertx, "Hello from clustered messenger example!");
         router.post("/sendForAll/:" + PATH_PARAM_TO_RECEIVE_MESSAGE).handler(this::sendMessageForAllReceivers);
+        router.post("/sendToNodeA/:" + PATH_PARAM_TO_RECEIVE_MESSAGE).handler(this::sendToNodeAOnly);
+        router.post("/sendToNodeB/:" + PATH_PARAM_TO_RECEIVE_MESSAGE).handler(this::sendToNodeBOnly);
         HttpServerHelper.createAnHttpServer(vertx, router, config(), promise);
     }
 
@@ -33,9 +35,31 @@ public class ClusteredSender extends AbstractVerticle
     private void sendMessageForAllReceivers(RoutingContext routingContext){
         final EventBus eventBus = vertx.eventBus();
         final String message = routingContext.request().getParam(PATH_PARAM_TO_RECEIVE_MESSAGE);
+        logger.info("Clustered SEND : {}",message);
         eventBus.publish(ADDRESS, message);
         logger.info("Current Thread Id {} Is Clustered {} ",
                 Thread.currentThread().getId(), vertx.isClustered());
         routingContext.response().end(message);
     }
+
+    private void sendToNodeAOnly(RoutingContext routingContext){
+        final EventBus eventBus = vertx.eventBus();
+        final String message = routingContext.request().getParam(PATH_PARAM_TO_RECEIVE_MESSAGE);
+        logger.info("Point to Point SEND to Node A : {}",message);
+        eventBus.send(NODEA_ADDRESS, message);
+        logger.info("Current Thread Id {} Is Clustered {} ",
+                Thread.currentThread().getId(), vertx.isClustered());
+        routingContext.response().end(message);
+    }
+
+    private void sendToNodeBOnly(RoutingContext routingContext){
+        final EventBus eventBus = vertx.eventBus();
+        final String message = routingContext.request().getParam(PATH_PARAM_TO_RECEIVE_MESSAGE);
+        logger.info("Point to Point SEND to Node B : {}",message);
+        eventBus.send(NODEB_ADDRESS, message);
+        logger.info("Current Thread Id {} Is Clustered {} ",
+                Thread.currentThread().getId(), vertx.isClustered());
+        routingContext.response().end(message);
+    }
+
 }
