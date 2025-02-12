a72d0ca092aa3f381274e69da9abd1e92b5a991b
Ashish Patel <ashmpatel@gmail.com>
2025-02-12 15:12:08 +0000

    Update ClusteredReceiver.java

--- src/main/java/io/vertx/ClusteredReceiver.java
+++ src/main/java/io/vertx/ClusteredReceiver.java
@@ -6,17 +6,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static io.vertx.Constants.ADDRESS;
+import static io.vertx.Constants.NODEA_ADDRESS;
+import static io.vertx.Constants.NODEB_ADDRESS;
 
 public class ClusteredReceiver extends AbstractVerticle {
 
     private final Logger logger = LoggerFactory.getLogger(ClusteredReceiver.class);
+
+    private String[] args;
+
+    public ClusteredReceiver(String [] args) {
+        this.args = args;
+    }
 
     @Override
     public void start() {
         final EventBus eventBus = vertx.eventBus();
         logger.info("Current Thread Id {} Is Clustered {} ",
                 Thread.currentThread().getId(), vertx.isClustered());
-        eventBus.consumer(ADDRESS, receivedMessage -> logger.info("Received message: {} by thread : {}", receivedMessage.body(), Thread.currentThread().getId()));
+        eventBus.consumer(ADDRESS, receivedMessage ->
+                logger.info("Received message: {} by thread : {}",
+                        receivedMessage.body(),
+                        Thread.currentThread().getId()));
+
+        if (args[0].equals("A")) {
+            eventBus.consumer(NODEA_ADDRESS, receivedMessage ->
+                    logger.info("Received by NODE - A message: {} by thread : {}",
+                            receivedMessage.body(),
+                            Thread.currentThread().getId()));
+        } else {
+            eventBus.consumer(NODEB_ADDRESS, receivedMessage ->
+                    logger.info("Received by NODE - B message: {} by thread : {}",
+                            receivedMessage.body(),
+                            Thread.currentThread().getId()));
+        }
+
         logger.info("Receiver ready!");
 
     }
